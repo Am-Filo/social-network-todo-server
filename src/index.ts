3c0976030988e02a087f57ab8f741c5fc6ba1711
Amary <laumiqv@gmail.com>
2020-07-23 11:40:46 +0300

    inpliment event subscribers

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ import { buildSchema } from "type-graphql";
 import { ApolloServer } from "apollo-server-express";
 import { createConnection } from "typeorm";
 
-// const resolver = require("./models/resolvers");
 import {
   UserResolver,
   ProfileResolver,
@@ -17,10 +16,23 @@ import {
   TodoListResolver,
   TodoItemResolver,
 } from "./models/resolvers";
+// import { User } from "./models/entity";
+// import { verify } from "jsonwebtoken";
 
+const http = require("http");
 const appRouter = require("./routes/app");
 
 const port = process.env.PORT || 4000;
+
+// const validateToken = (authToken) => {
+//   // ... validate token and return a Promise, rejects in case of an error
+// };
+
+// const findUser = (authToken) => {
+//   return (tokenValidationResult) => {
+//     // ... finds user by auth token and return a Promise, rejects in case of an error
+//   };
+// };
 
 (async () => {
   const app = express();
@@ -49,11 +61,45 @@ const port = process.env.PORT || 4000;
       ],
     }),
     context: ({ req, res }) => ({ req, res }),
+    subscriptions: {
+      path: "/graphql",
+      onConnect: async (_connectionParams, _webSocket) => {
+        console.log(
+          `Subscription client connected using Apollo server's built-in SubscriptionServer.`
+        );
+
+        // console.log(connectionParams, webSocket);
+
+        // if (connectionParams.authToken) {
+        //   return validateToken(connectionParams.authToken)
+        //     .then(findUser(connectionParams.authToken))
+        //     .then((user) => {
+        //       return {
+        //         currentUser: user,
+        //       };
+        //     });
+        // }
+
+        // throw new Error("Missing auth token!");
+      },
+      onDisconnect: async (_webSocket, _context) => {
+        // console.log(webSocket, context);
+        console.log(`Subscription client disconnected.`);
+      },
+    },
   });
 
   apolloServer.applyMiddleware({ app, cors: false });
 
-  app.listen(port, () => {
-    console.log(`express server started on port ${port}`);
+  const httpServer = http.createServer(app);
+  apolloServer.installSubscriptionHandlers(httpServer);
+
+  httpServer.listen(port, () => {
+    console.log(
+      `🚀 Server ready at http://localhost:${port}${apolloServer.graphqlPath}`
+    );
+    console.log(
+      `🚀 Subscriptions ready at ws://localhost:${port}${apolloServer.subscriptionsPath}`
+    );
   });
 })();
